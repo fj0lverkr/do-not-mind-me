0aeed4a903f3f08e03a2a2f8bd2c6cc3b2aa7de1
Nils Nahooy <nils.nahooy@live.com>
2024-10-09 20:05:09 +0200

    fix player rotation

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -15,7 +15,9 @@ func _physics_process(_delta: float) -> void:
 
 
 func _process_input() -> void:
-    var new_velocity: Vector2 = Vector2.ZERO
-    new_velocity.x = Input.get_action_strength("right") - Input.get_action_strength("left")
-    new_velocity.y = Input.get_action_strength("down") - Input.get_action_strength("up")
-    velocity = new_velocity.normalized() * _speed
+    velocity.x = Input.get_action_strength("right") - Input.get_action_strength("left")
+    velocity.y = Input.get_action_strength("down") - Input.get_action_strength("up")
+    velocity = velocity.normalized()
+    velocity *= _speed
+    if Input.is_action_pressed("up") or Input.is_action_pressed("down") or Input.is_action_pressed("left") or Input.is_action_pressed("right"):
+        rotation = velocity.angle()
