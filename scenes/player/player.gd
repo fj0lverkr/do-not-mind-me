c2662564512b254c8809692031bafd1816ddbad0
Nils Nahooy <nils.nahooy@live.com>
2024-10-09 17:46:01 +0200

    player movement

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,10 +1,21 @@
 class_name Player
 extends CharacterBody2D
 
+@export
+var _speed: float = 130.0
+
 
 func _ready() -> void:
     pass
 
 
 func _physics_process(_delta: float) -> void:
-    pass
+    _process_input()
+    move_and_slide()
+
+
+func _process_input() -> void:
+    var new_velocity: Vector2 = Vector2.ZERO
+    new_velocity.x = Input.get_action_strength("right") - Input.get_action_strength("left")
+    new_velocity.y = Input.get_action_strength("down") - Input.get_action_strength("up")
+    velocity = new_velocity.normalized() * _speed
